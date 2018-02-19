20697d6279b33d5dd02bbfedfa6917f07d3d68ef
Victoria West <victoriawest811@gmail.com>
2018-02-19 16:00:52 -0500

    Question 4

--- src/com/company/Question_4/Main.java
+++ src/com/company/Question_4/Main.java
@@ -1,6 +1,11 @@
 package com.company.Question_4;
 
+import java.util.Scanner;
+
 public class Main {
+
+    private static Scanner input;
+    private Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
         /*
@@ -10,6 +15,27 @@ public class Main {
         teleported to the beach".  If the number is between 91 and/or equal to 100 display, "Well, I hope you like it hot,
         you have been teleported to a volcano."
          */
+
+        System.out.println("Enter a number between 1 and 100");
+        String userInput = input.nextLine();
+
+
+
+        if (userInput >= 1 && userInput <= 45);
+        System.out.println("You have been teleported to a swamp");
+        else if (userInput >= 46 && userInput <= 90);
+        System.out.println("Lucky you, you've been teleported to the beach");
+        else (userInput >= 91 && userInput <= 100);
+        System.out.println("Well, I hope you like it hot, you have been teleported to a volcano.");
+
+
+
+
+
+
+
+
+
     }
 
 }
