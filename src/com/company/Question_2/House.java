1f9a57e351153891e5e6d073698a31f4d91a2378
Victoria West <victoriawest811@gmail.com>
2018-02-19 15:04:28 -0500

    Question 2

---
+++ src/com/company/Question_2/House.java
@@ -0,0 +1,30 @@
+package com.company.Question_2;
+
+public class House {
+
+    private int age;
+    private String color;
+
+    public House(int age, String color) {
+        this.age = age;
+        this.color = color;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
+
+
--- src/com/company/Question_2/Main.java
+++ src/com/company/Question_2/Main.java
@@ -7,6 +7,9 @@ public class Main {
         1. Create a new class in this package that will hold a model for a house (at least 2 Variables, getters, setters and constructor)
         2. Create an instance of the house in the main method here.
          */
+
+
+        House house = new House(20, "white");
     }
 
 }
